7afd0c2248585fa815727e9060040584ae1a1cbd
HuyHeox <ngoquanghuypro@gmail.com>
2022-05-19 21:06:33 +0700

    lab6

---
+++ lab6/week6/lab6-2/ex6.5/ex5.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="ex5.css"></link>
+    </head>
+    <body>
+        <header>Header content</header>
+        <section>
+            <article>
+                <h1>Main content</h1>
+                <div>
+                    Main contentMain contentMain contentMain content
+                </div>
+            </article>
+            <side-bar>
+                <p>Sidebar content</p>
+                <p>Sidebar contentSidebar</p>
+                <p>contentSidebar contentSidebar </p>
+                <p>contentSidebar contentSidebar</p>
+                <p>content</p>
+            </side-bar>
+        </section>
+        <div style="text-align:center;">
+        ...
+        </div>
+        
+        <section>
+            <article></article>
+            <side-bar style="height:100px; width:195px"></side-bar>
+        </section>
+        <footer>
+            Footer content
+        </footer>
+    </body>
+</html>
