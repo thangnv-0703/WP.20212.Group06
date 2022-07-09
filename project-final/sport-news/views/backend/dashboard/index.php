c91238eada3d48e15808c34ee3e0017622adc472
Leo Nguyen <leo.nguyen@dssolution.jp>
2022-07-09 22:36:05 +0700

    add common layout backend screen

---
+++ project-final/sport-news/views/backend/dashboard/index.php
@@ -0,0 +1,17 @@
+<?php require_once('views/backend/includes/header.php') ?>
+<div class="col-md-9">
+    <div class="col-md-12 page-body">
+      <div class="row">
+        <div class="sub-title">
+              <h2>Danh mục</h2>
+                <a href="contact.html"><i class="icon-envelope"></i></a>
+                <p><a href="index.php?type=backend&mod=post&act=create">Bảng </a></p>
+            </div>
+              <div class="col-md-12 content-page">
+                <div class="col-md-12 blog-post">
+                  <p>Trang quản lý bài viết, danh mục và người viết</p>   
+                </div>
+              </div>
+    </div>
+</div>
+<?php require_once('views/backend/includes/footer.php') ?>                        
---
+++ project-final/sport-news/views/backend/includes/footer.php
@@ -0,0 +1,42 @@
+                     
+<!-- Footer Start -->
+                       <div class="col-md-12 page-body margin-top-50 footer">
+                          <footer>
+                          <p>© Copyright 2021 LowKey Blog. All rights reserved</p>
+                          
+                          
+                          <!-- UiPasta Credit Start -->
+                          <div class="uipasta-credit">Design By <a href="http://www.uipasta.com" target="_blank">Nguyen Duc Hieu</a></div>
+                          <!-- UiPasta Credit End -->
+
+                           
+                         </footer>
+                       </div>
+                       <!-- Footer End -->
+                     
+                     
+                  </div>
+                  <!-- Blog Post (Right Sidebar) End -->
+                
+            </div>
+         </div>
+      </div>
+    
+    
+    
+    <!-- Back to Top Start -->
+    <a href="#" class="scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <!-- Back to Top End -->
+    
+    
+    <!-- All Javascript Plugins  -->
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugin.js"></script>
+    
+    <!-- Main Javascript File  -->
+    <script type="text/javascript" src="assets/js/scripts.js"></script>
+
+    
+   </body>
+ </html>
+
